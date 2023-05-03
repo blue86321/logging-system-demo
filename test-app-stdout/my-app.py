8383367bdfc9baab82f9e1093482f75a4f4bd1be
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-05-02 23:23:55 -0700

    style: naming, translation, docs

--- test-app-stdout/my-app.py
+++ test-app-stdout/my-app.py
@@ -1,14 +1,14 @@
 import random
 import time
-import GgoLogging
+import MyAppLogging
 
 while True:
     # `order` message
-    GgoLogging.log_order()
+    MyAppLogging.log_order()
     
     # 30% chance to send a `login` message
     if (random.randint(1, 10) < 3):
-        GgoLogging.log_login()
+        MyAppLogging.log_login()
 
     # sleep for 0 - 30 seconds
     time.sleep(random.random() * 30)
