70747c084face9222c6d86dcfb242d09e515385c
Chunwei Chang <chunwei.chang.cwc@gmail.com>
2023-06-09 18:34:10 -0700

    feat: tf output dashboard endpoint
    Former-commit-id: b6bf8289b8eab55d5766640f8d75ab5e6402ba43 [formerly 10f67f631bf1b05062ee1ac8eecac19a0cf55a9d]
    Former-commit-id: e2735e580f7548fdfc290116fe26e5eb48bca426

--- tf/modules/opensearch/outputs.tf
+++ tf/modules/opensearch/outputs.tf
@@ -5,3 +5,7 @@ output "opensearch_endpoint" {
 output "opensearch_master_role_arn" {
   value = aws_iam_role.auth_master.arn
 }
+
+output "opensearch_dashboard_endpoint" {
+  value = aws_opensearch_domain.this.dashboard_endpoint
+}
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -17,6 +17,10 @@ output "AWS_OPENSEARCH_HOST" {
   value = module.opensearch.opensearch_endpoint
 }
 
+output "AWS_OPENSEARCH_DASHBOARD" {
+  value = module.opensearch.opensearch_dashboard_endpoint
+}
+
 output "AWS_OPENSEARCH_ROLE_ARN" {
   value = module.opensearch.opensearch_master_role_arn
 }
